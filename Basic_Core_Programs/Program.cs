95aa72042ae6a0a8152af37f823e5108519b1f5d
Anurag1020 <anurag.mendhe14@gmail.com>
2022-07-08 15:48:22 +0530

    Leap_Year

--- Basic_Core_Programs/Program.cs
+++ Basic_Core_Programs/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Enter year:-");
+int year = Convert.ToInt32(Console.ReadLine());
+
+if((year%4==0)&&(year%100!=0 || (year%400==0)))
+{
+    Console.WriteLine(year + "is a leap year");
+}
+else
+{
+    Console.WriteLine(year + "is not a leap year");
+}
