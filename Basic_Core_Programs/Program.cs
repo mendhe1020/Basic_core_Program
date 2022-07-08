45bf4e3d363c181c1b3063dcfb9efc3b8eef0f85
Anurag1020 <anurag.mendhe14@gmail.com>
2022-07-08 15:35:54 +0530

    Flip_coin

--- Basic_Core_Programs/Program.cs
+++ Basic_Core_Programs/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Random random = new Random();
+int coin = random.Next(0, 2);
+
+if(coin==0)
+{
+    Console.WriteLine("Head");
+}
+else
+{
+    Console.WriteLine("Tails");
+}
